2e25842217946b68abae148117046b55a483ad6a
Cole Smith <smithcole@protonmail.com>
2023-07-25 09:44:50 -0700

    good stuff

--- src/pages/print.tsx
+++ src/pages/print.tsx
@@ -35,14 +35,12 @@ const shareFile = (file: any, title: string, text: string) => {
 
 const Reciept = ({ items, total }: { items: CartStockItem[], total: number }) => {
     const stageRef = React.useRef(null);
-
     const handleExport = () => {
         //@ts-ignore
         if (stageRef.current && stageRef.current.toDataURL) {
             //@ts-ignore
             const uri = stageRef.current.toImage({
                 callback(img: any) {
-                    console.log(img.src)
                     setImg(img.src)
                 }
             });
@@ -59,57 +57,63 @@ const Reciept = ({ items, total }: { items: CartStockItem[], total: number }) =>
         if (stageRef.current && stageRef.current.toDataURL) {
             //@ts-ignore
             const uri = stageRef.current.toDataURL();
-            const file = dataURLtoFile(uri, "thanku_poster.png");
-            shareFile(file, "Title", "https://co-aid.in");
+            const file = dataURLtoFile(uri, "boardsource receipt.png");
+            shareFile(file, "Receipt", "boardsource  receipt");
         }
     }
     const [img, setImg] = useState(null)
     const getY = (i: number) => {
-        return i * 30 + 65
+        return i * 30 + 90
     }
-    const height = items.length * 30 + 120
+    const height = items.length * 30 + 180
+
 
     useEffect(() => {
-        handleExport()
+        setTimeout(() => {
+            handleExport()
+
+            sendToPrint()
+        }, 1000)
+
     }, [])
     const getTax = () => {
         return (total * 8.3) / 100
     }
 
     return (
-        <div className="flex  justify-between">
+        <div className="flex flex-col-reverse items-center	">
             <Stage width={200} height={height} ref={stageRef}>
                 <Layer >
                     <Rect
                         x={0}
                         y={0}
                         width={200}
-                        height={items.length * 100}
+                        height={height}
                         fill={"white"}
                     />
                     <Circle
-                        x={20}
-                        y={20}
+                        x={25}
+                        y={25}
                         radius={18}
                         strokeWidth={3}
                         stroke={"black"}
 
                     />
                     <Circle
-                        x={20}
-                        y={20}
+                        x={25}
+                        y={25}
                         radius={3}
                         stroke={"black"}
                         fill="black"
                     />
-                    <Text x={45} y={10} fontSize={25} text={`Boardsource`} />
+                    <Text x={45} y={15} fontSize={25} text={`Boardsource`} />
 
-                    <Text x={2} y={39} fontSize={20} text={`item`} />
-                    <Text x={125} y={39} fontSize={20} text={`qty`} />
-                    <Text x={155} y={39} fontSize={20} text={`price`} />
+                    <Text x={2} y={45} fontSize={20} text={`item`} />
+                    <Text x={125} y={45} fontSize={20} text={`qty`} />
+                    <Text x={155} y={45} fontSize={20} text={`price`} />
                     <Rect
                         x={0}
-                        y={60}
+                        y={70}
                         width={200}
                         height={2}
                         fill={"black"}
@@ -118,37 +122,37 @@ const Reciept = ({ items, total }: { items: CartStockItem[], total: number }) =>
                 </Layer>
                 {items.map((item, i) => (
                     <Layer key={i}>
-                        <Text x={2} y={getY(i)} fontSize={20} text={`${item.name}`} />
-                        <Text x={145} y={getY(i)} fontSize={20} text={`${item.quantity}`} />
-                        <Text x={165} y={getY(i)} fontSize={20} text={`$${item.price}`} />
-
-                        <Text text={item.buildGuide} />
+                        <Text x={2} y={getY(i)} fontSize={18} text={`${item.name}`} />
+                        <Text x={145} y={getY(i)} fontSize={18} text={`${item.quantity}`} />
+                        <Text x={165} y={getY(i)} fontSize={18} text={`$${item.price}`} />
+                        {/* <Text x={2} y={getY(i) + 15} fontSize={10} text={item.buildGuide} /> */}
                     </Layer>
                 ))}
                 <Layer>
                     <Rect
                         x={0}
-                        y={height - 65}
+                        y={height - 80}
                         width={200}
                         height={2}
                         fill={"black"}
                     />
-                    <Text x={2} y={height - 60} fontSize={20} text={`subTotal`} />
-                    <Text x={150} y={height - 60} fontSize={20} text={`$${total}`} />
-                    <Text x={2} y={height - 40} fontSize={20} text={`Tax`} />
-                    <Text x={150} y={height - 40} fontSize={20} text={`$${getTax()}`} />
-                    <Text x={2} y={height - 20} fontSize={20} text={`total`} />
-                    <Text x={150} y={height - 20} fontSize={20} text={`$${Math.floor(getTax() + total)}`} />
+                    <Text x={2} y={height - 75} fontSize={20} text={`subTotal`} />
+                    <Text x={150} y={height - 75} fontSize={20} text={`$${total}`} />
+                    <Text x={2} y={height - 52} fontSize={20} text={`Tax`} />
+                    <Text x={150} y={height - 52} fontSize={20} text={`$${getTax()}`} />
+                    <Text x={2} y={height - 30} fontSize={20} text={`total`} />
+                    <Text x={150} y={height - 30} fontSize={20} text={`$${Math.floor(getTax() + total)}`} />
                 </Layer>
 
             </Stage>
-            {img && <img src={img} alt="" />}
+            {/* {img && <img src={img} alt="" />} */}
             <button
-                className="px-2 py-1 bg-green-500 text-white rounded"
+                className="px-2 py-1 bg-blue-500 text-white rounded  mb-2"
                 onClick={sendToPrint}
             >
                 sendToPrint
             </button>
+
         </div>
     )
 }
@@ -158,7 +162,7 @@ export default function print(props: any) {
     const total: number | undefined = props.location && props.location.state && props.location.state.total ? props.location.state.total : undefined
     return (
         <div className="flex flex-col">
-            <div className="flex justify-between mb-10">
+            <div className="flex justify-around mb-10">
                 <h1 className="text-xl">
                     Reciept
                 </h1>
@@ -170,7 +174,7 @@ export default function print(props: any) {
             </div>
 
             <div className="pattern-zigzag-3d pattern-blue-500 pattern-bg-white 
-  pattern-size-6 pattern-opacity-20 bg-blue-200 min-h-full">
+  pattern-size-6 pattern-opacity-20  min-h-full">
 
                 {items && total ? <Reciept items={Object.values(items)} total={total} /> : <div>no items</div>}
             </div>
